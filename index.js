1dfdaeabcae99351be9a72872027705fccb5077c
ergasap <ernestogaspar2000@gmail.com>
2023-05-10 11:19:28 +0200

    First aproximation

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get("/static", (req, res) => {
+    res.render("static");
 });
 
 app.listen(port, () => {
